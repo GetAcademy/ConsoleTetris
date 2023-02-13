5a03476d2a3496ab684174a37a4104b6feeb5f39
GET Academy <terje@getacademy.no>
2023-02-13 13:26:31 +0100

    Initial commit

---
+++ ConsoleTetris/Grid.cs
@@ -0,0 +1,22 @@
+﻿namespace ConsoleTetris
+{
+    internal class Grid
+    {
+        private readonly ConsoleColor?[] _cells;
+        private readonly int _width;
+        private readonly int _height;
+
+        public Grid(int width, int height)
+        {
+            _height = height;
+            _width = width;
+            var cellCount = width * height;
+            _cells = new ConsoleColor?[cellCount];
+        }
+
+        public void SetColor(ConsoleColor color, int index)
+        {
+            _cells[index] = color;
+        }
+    }
+}
---
+++ ConsoleTetris/Piece.cs
@@ -0,0 +1,18 @@
+﻿namespace ConsoleTetris
+{
+    internal class Piece : Grid
+    {
+        public Piece(int size) : base(size, size)
+        {
+        }
+
+        public Piece(int size, ConsoleColor color, params int[] indexes)
+        : this(size)
+        {
+            foreach (var index in indexes)
+            {
+                SetColor(color, index);
+            }
+        }
+    }
+}
